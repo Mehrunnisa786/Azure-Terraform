66f4a898c0f5e6a1bab58bedd7f98a612ef06d75
Mehrunnisa <mehrunnisaafrah@gmail.com>
2026-06-28 18:50:46 +0000

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "demo-resource"
-    storage_account_name = "trainwithafrah"
+    storage_account_name = "trainwithafrahh"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
